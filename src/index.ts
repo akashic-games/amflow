6784903ac4b2b91ed5f158f59092e821efede374
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2021-01-26 11:07:24 +0900

    Add akashic/eslint-config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as AMFlow from "./AMFlow";
-import * as StartPoint from "./StartPoint";
-import * as Permission from "./Permission";
 import * as Error from "./Error";
+import * as Permission from "./Permission";
+import * as StartPoint from "./StartPoint";
 
 export import GetStartPointOptions = AMFlow.GetStartPointOptions;
 export import GetTickListOptions = AMFlow.GetTickListOptions;
